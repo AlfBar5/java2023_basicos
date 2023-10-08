6451a6ec7640a4b7771f766b4eef8b47a45dceca
ALF <ALF@DESKTOP-KIMKS00>
2023-10-08 11:15:34 +0200

    Cambio para pruebas en 01_bienvenida Cambiada edad a 39

--- 01_bienvenida/src/principal/Saludo.java
+++ 01_bienvenida/src/principal/Saludo.java
@@ -6,7 +6,7 @@ public class Saludo {
 	//(la JVM va a leer el main siempre) (como mínimo tiene que estar el main)
 	public static void main(String[] args) {
 		// Mostra algo por pantalla
-		int vida=29;
+		int vida=39;
 		System.out.println("Bienvenido a Java");
 		System.out.println("Java tiene "+vida+" años");
 
